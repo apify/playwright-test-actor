fb2fd77100ea14a45c8e92623cd972a6276b1c74
Jindřich Bär <jindrichbar@gmail.com>
2023-02-23 15:14:33 +0100

    feat: recording test videos

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 import { Actor } from 'apify';
 import log from '@apify/log';
 import { Dictionary } from 'apify-client';
-import { writeFileSync, readFileSync } from 'fs';
+import fs from 'fs';
 import path from 'path';
 import { execSync } from 'child_process';
-import { transformToTabular } from './transform';
+import { collectAttachmentPaths, transformToTabular } from './transform';
 
-const getConfig = (options: {screen: {width: number, height: number}, headful: boolean, timeout: number, locale: string, darkMode: boolean, ignoreHTTPSErrors: boolean}) => {
-    const {screen, headful, timeout, ignoreHTTPSErrors, darkMode, locale} = options;
+const getConfig = (options: {screen: {width: number, height: number}, headful: boolean, timeout: number, locale: string, darkMode: boolean, ignoreHTTPSErrors: boolean, video: string}) => {
+    const {screen, headful, timeout, ignoreHTTPSErrors, darkMode, locale, video} = options;
 
     return `import { defineConfig } from '@playwright/test';
 export default defineConfig({
@@ -16,8 +16,9 @@ export default defineConfig({
         headless: ${!headful},
         viewport: { width: ${screen.width}, height: ${screen.height} },
         ignoreHTTPSErrors: ${ignoreHTTPSErrors},
-        colorSchema: '${darkMode ? 'dark' : 'light'}',
+        colorScheme: '${darkMode ? 'dark' : 'light'}',
         locale: '${locale}',
+        video: '${video}',
     },
     reporter: [
         ['html'],
@@ -35,7 +36,7 @@ function runTests() {
 }
 
 function storeTestCode(args: { contents: string, path: string }) {
-    return writeFileSync(args.path, args.contents as string, { encoding: 'utf-8' });
+    return fs.writeFileSync(args.path, args.contents as string, { encoding: 'utf-8' });
 }
 
 function updateConfig(args: {
@@ -46,6 +47,7 @@ function updateConfig(args: {
     darkMode?: boolean,
     locale?: string,
     ignoreHTTPSErrors?: boolean,
+    video?: string,
 }) {
     const { 
         screenWidth = 1280, 
@@ -55,10 +57,11 @@ function updateConfig(args: {
         darkMode = false,
         locale = 'en-US',
         ignoreHTTPSErrors = true,
+        video = 'off'
     } = args;
 
-    const config = getConfig({screen: { width: screenWidth, height: screenHeight }, headful, timeout: timeout * 1000, locale, darkMode, ignoreHTTPSErrors});
-    writeFileSync(path.join(__dirname, 'playwright.config.ts'), config, { encoding: 'utf-8' });
+    const config = getConfig({screen: { width: screenWidth, height: screenHeight }, headful, timeout: timeout * 1000, locale, darkMode, ignoreHTTPSErrors, video});
+    fs.writeFileSync(path.join(__dirname, 'playwright.config.ts'), config, { encoding: 'utf-8' });
 }
 
 (async () => {
@@ -71,16 +74,24 @@ function updateConfig(args: {
     });
 
     updateConfig(input);
+
     runTests();
 
     const kvs = await Actor.openKeyValueStore();
-    await kvs.setValue('report', readFileSync(path.join(__dirname, 'playwright-report', 'index.html'), { encoding: 'utf-8' }), { contentType: 'text/html' });
+    await kvs.setValue('report', fs.readFileSync(path.join(__dirname, 'playwright-report', 'index.html'), { encoding: 'utf-8' }), { contentType: 'text/html' });
 
+    const jsonReport = JSON.parse(fs.readFileSync(path.join(__dirname, 'test-results.json'), { encoding: 'utf-8' }));
+    const attachmentPaths = collectAttachmentPaths(jsonReport);
+
+    const attachmentLinks = await Promise.all(attachmentPaths.map(async (x) => {
+        const attachment = fs.readFileSync(x.path);
+        await kvs.setValue(x.key, attachment, { contentType: x.type ?? 'application/octet' });
+        return {...x, url: await kvs.getPublicUrl(x.key)};
+    }));
+
+    await Actor.pushData(transformToTabular(jsonReport, attachmentLinks));
+    
     const reportURL = await kvs.getPublicUrl('report');
-
-    const jsonReport = JSON.parse(readFileSync(path.join(__dirname, 'test-results.json'), { encoding: 'utf-8' }));
-    await Actor.pushData(transformToTabular(jsonReport));
-
     log.info('The test run has finished! The report is available in the Output tab or at the link below:');
     console.log(reportURL);
     
--- transform.ts
+++ transform.ts
@@ -1,10 +1,13 @@
-export function transformToTabular(testResults: Record<string, any>) {
+import { v4 } from 'uuid';
+
+export function transformToTabular(testResults: Record<string, any>, attachmentLinks: Attachment[]): any[] {
     const acc: any[] = [];
 
     for (let suite of testResults.suites) {
         for (let test of suite.specs) {
             for (let runner of test.tests) {
                 const { results } = runner;
+
                 acc.push({
                     suiteName: suite.title,
                     testName: test.title,
@@ -12,10 +15,38 @@ export function transformToTabular(testResults: Record<string, any>) {
                     result: runner.status === 'expected',
                     errors: results.reduce((p: string[], e: {errors: {message: string}[]}) => [...p, ...e.errors.map(e => e.message)], []),
                     duration: results.reduce((acc: number, curr: {duration: number}) => acc + curr.duration, 0),
+                    video: attachmentLinks.find(x => results.reduce((p: string[], x: {attachments: { path:string }[]}) => [...p, ...x.attachments.map(x => x.path)], []).includes(x.path))?.url,
                 })
             }
         }
     }
+
+    return acc;
+}
+
+interface Attachment {
+    key: string, 
+    path: string, 
+    type: string,
+    url?: string,
+};
+
+function _collectAttachmentPaths(acc: Attachment[], testResults: Record<string, any>) {
+    for(let e of Object.entries(testResults)) {
+        const [key, value] = e;
+        if (key === 'attachments') {
+            for(let attachment of value) {
+                acc.push({ path: attachment.path, type: attachment.contentType, key: v4() });
+            }
+        } else if (typeof value === 'object' && value !== null) {
+            _collectAttachmentPaths(acc, value);
+        }
+    }
+}
+
+export function collectAttachmentPaths(testResults: Record<string, any>): Attachment[] {
+    const acc: any[] = [];
+    _collectAttachmentPaths(acc, testResults);
 
     return acc;
 }
